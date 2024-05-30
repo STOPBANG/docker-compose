df1071927055322d1e45100bb30c2e484b546ca5
ywonchae1 <ywonchae62@gmail.com>
2024-05-30 16:10:17 +0900

    feat: 각 ms 도커 이미지 빌드 스크립트

---
+++ script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 도커 이미지 빌드하는 스크립트
+# 무조건 stop_bang_msa 디렉토리 위치에서 실행할 것
+
+PRE="stopbang-"
+
+for dir in $PWD/*
+do
+	if [ -d $dir ]; then
+		cd $dir
+		BASENAME=$(basename "$PWD")
+		PICK=${BASENAME#*stop_bang_}
+		IMAGENAME=""
+		if [ $PICK = "auth_DB" ]; then
+			IMAGENAME="ywonchae1/stopbang-auth-api"
+		elif [ $PICK = "review_DB" ]; then
+			IMAGENAME="ywonchae1/stopbang-review-api"
+		elif [ $PICK = "sub_DB" ]; then
+			IMAGENAME="ywonchae1/stopbang-sub-api"
+		elif [ $PICK = "login_logout" ]; then
+			IMAGENAME="ywonchae1/stopbang-login-logout-api"
+		else
+			IMAGENAME="ywonchae1/stopbang-${PICK}-ms"
+		fi
+		docker build --platform linux/amd64 -t $IMAGENAME .
+		docker push $IMAGENAME
+		cd ..
+	fi
+done
